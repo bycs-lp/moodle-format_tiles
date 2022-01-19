24f8ce6380490337167fb59185861218041a27bd
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-01-19 10:45:24 +0100

    MBS-6098: Change labellike detection to url attribute of cm_info

--- lib.php
+++ lib.php
@@ -40,16 +40,7 @@ require_once($CFG->dirroot . '/course/format/lib.php');
  * @copyright 2016 David Watson {@link http://evolutioncode.uk}
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class format_tiles extends core_courseformat\base {
-
-    /**
-     *  We want to treat label and plugins that behave like labels as labels.
-     * E.g. we don't render them as subtiles but show their content directly on page.
-     * This includes plugins like mod_customlabel and mod_unilabel, as defined here.
-     * @var []
-     */
-    public $labellikecoursemods = ['label', 'customlabel', 'unilabel', 'datalynxcoursepage'];
-
+class format_tiles extends format_base {
     /**
      * Creates a new instance of class
      *
