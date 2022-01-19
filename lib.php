354072de18739bfb3487d43f9a1acd33e8982d16
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-01-19 17:54:33 +0100

    MBS-6109: Add highlighting for new activities

--- lib.php
+++ lib.php
@@ -380,6 +380,10 @@ class format_tiles extends core_courseformat\base {
                 'courseusebarforheadings' => array(
                     'default' => 1,
                     'type' => PARAM_INT,
+                ),
+                'courseshownewactivities' => array(
+                    'default' => 1,
+                    'type' => PARAM_INT,
                 )
             );
             if ((get_config('format_tiles', 'followthemecolour'))) {
@@ -483,6 +487,15 @@ class format_tiles extends core_courseformat\base {
                 'element_type' => 'advcheckbox',
                 'element_attributes' => array(get_string('yes')),
                 'help' => 'courseusebarforheadings',
+                'help_component' => 'format_tiles',
+            );
+            $courseformatoptionsedit['courseshownewactivities'] = array(
+                'label' => new lang_string(
+                    'courseshownewactivities', 'format_tiles'
+                ),
+                'element_type' => 'advcheckbox',
+                'element_attributes' => array(get_string('yes')),
+                'help' => 'courseshownewactivities',
                 'help_component' => 'format_tiles',
             );
             if ($allowsubtilesview) {
