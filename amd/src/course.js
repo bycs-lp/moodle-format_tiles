243999aa1b4861bc8436b88a21d4a25404c896b6
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-08-21 06:50:25 +0200

    MBS-6622: Transfer JS via AJAX

--- amd/src/course.js
+++ amd/src/course.js
@@ -555,13 +555,13 @@ define(["jquery", "core/templates", "core/ajax", "format_tiles/browser_storage",
 
                 // Still contact the server in case content has changed (e.g. restrictions now satisfied).
                 getSectionContentFromServer(courseId, dataSection).done(function (response) {
-                    setCourseContentHTML(relatedContentArea, $(response.html).html());
+                    setCourseContentHTML(relatedContentArea, $(response.html).html() + response.js);
                 });
             } else {
                 relatedContentArea.html(loadingIconHtml);
                 // Get from server.
                 getSectionContentFromServer(courseId, dataSection).done(function (response) {
-                    setCourseContentHTML(relatedContentArea, $(response.html).html());
+                    setCourseContentHTML(relatedContentArea, $(response.html).html() + response.js);
                     expandSection(relatedContentArea, dataSection);
                 }).fail(function (failResult) {
                     failedLoadSectionNotify(dataSection, failResult, relatedContentArea);
@@ -659,7 +659,7 @@ define(["jquery", "core/templates", "core/ajax", "format_tiles/browser_storage",
                                 getSectionContentFromServer(courseId, dataSection + 1).done(function(response) {
                                     setCourseContentHTML(
                                         nextSecIfExists,
-                                        $(response.html).html()
+                                        $(response.html).html() + response.js
                                     );
                                 });
                             }
@@ -752,7 +752,10 @@ define(["jquery", "core/templates", "core/ajax", "format_tiles/browser_storage",
                         ]);
                         requests[0]
                             .done((response) => {
-                                setCourseContentHTML($(Selector.SECTION_ID + data.section), $(response.html).html());
+                                setCourseContentHTML(
+                                    $(Selector.SECTION_ID + data.section),
+                                    $(response.html).html() + response.js
+                                );
                             })
                             .catch(err => {
                                 require(["core/log"], function(log) {
